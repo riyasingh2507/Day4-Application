9fc09f26d7a0d4b3b5b92a1c12aa48111a64b50a
Riya Singh <riyanita2000@gmail.com>
2023-05-01 13:53:05 +0530

    Day 4 Angular Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,149 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Day4-Application';
+  fname!: string;
+  lname!: string;
+  amount!: string;
+  myCurrency!: string;
+  show:boolean=false;
+
+  selectChangeHandler(event: any) {
+    this.myCurrency = event.target.value;
+  }
+
+  searchTerm: string = '';
+
+  users: any = [
+    {
+      Company: 'Skiba',
+      Contact: 'Pipelayer',
+      Country: 'Indonesia',
+    },
+    {
+      Company: 'Linkbuzz',
+      Contact: 'Safety Officer',
+      Country: 'Peru',
+    },
+    {
+      Company: 'Riffwire',
+      Contact: 'Painter',
+      Country: 'Brazil',
+    },
+    {
+      Company: 'Flashpoint',
+      Contact: 'Millwright',
+      Country: 'China',
+    },
+    {
+      Company: 'Skilith',
+      Contact: 'HVAC',
+      Country: 'Philippines',
+    },
+    {
+      Company: 'Dablist',
+      Contact: 'Refridgeration',
+      Country: 'Russia',
+    },
+    {
+      Company: 'Thoughtblab',
+      Contact: 'Millwright',
+      Country: 'Japan',
+    },
+    {
+      Company: 'Eire',
+      Contact: 'Plumber',
+      Country: 'Indonesia',
+    },
+    {
+      Company: 'Bubblemix',
+      Contact: 'Terrazzo',
+      Country: 'United States',
+    },
+    {
+      Company: 'Mycat',
+      Contact: 'Boilermaker',
+      Country: 'Philippines',
+    },
+    {
+      Company: 'Myworks',
+      Contact: 'Glazier',
+      Country: 'China',
+    },
+    {
+      Company: 'Eadel',
+      Contact: 'Stucco Mason',
+      Country: 'Indonesia',
+    },
+    {
+      Company: 'Kimia',
+      Contact: 'Tile Setter',
+      Country: 'China',
+    },
+    {
+      Company: 'Dabjam',
+      Contact: 'Terrazzo',
+      Country: 'Portugal',
+    },
+    {
+      Company: 'Jayo',
+      Contact: 'Brickmason',
+      Country: 'France',
+    },
+    {
+      Company: 'Dabfeed',
+      Contact: 'Painter',
+      Country: 'Russia',
+    },
+    {
+      Company: 'Rhynyx',
+      Contact: 'Linemen',
+      Country: 'Greece',
+    },
+    {
+      Company: 'Oba',
+      Contact: 'Pipefitter',
+      Country: 'Nepal',
+    },
+    {
+      Company: 'Twiyo',
+      Contact: 'Millwright',
+      Country: 'Philippines',
+    },
+    {
+      Company: 'Voonte',
+      Contact: 'Refridgeration',
+      Country: 'Russia',
+    },
+    {
+      Company: 'Rhybox',
+      Contact: 'Boilermaker',
+      Country: 'Ukraine',
+    },
+    {
+      Company: 'Quimba',
+      Contact: 'Welder',
+      Country: 'China',
+    },
+    {
+      Company: 'Midel',
+      Contact: 'Safety Officer',
+      Country: 'Guam',
+    },
+    {
+      Company: 'Kwinu',
+      Contact: 'Linemen',
+      Country: 'Philippines',
+    },
+    {
+      Company: 'Zoonder',
+      Contact: 'Carpenter',
+      Country: 'Iraq',
+    },
+  ];
+
+  constructor() {}
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FilterPipe} from '../models/filters';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
